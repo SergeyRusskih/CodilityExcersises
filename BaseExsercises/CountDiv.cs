c566218de1f50d65750560c9a3cbc9cf201ff9bd
Sergey <sergeivrusskikh@gmail.com>
2018-04-19 00:07:49 +0300

    some new exsercises

---
+++ BaseExsercises/CountDiv.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace BaseExsercises
+{
+    public class CountDiv
+    {
+        public int solution(int A, int B, int K)
+        {
+            return A % K > 0
+                ? (B / K - A / K)
+                : (B / K - A / K) + 1;
+        }
+
+        [Theory]
+        [InlineData(6, 11, 2, 3)]
+        [InlineData(2, 6, 2, 3)]
+        [InlineData(6, 7, 2, 1)]
+        [InlineData(11, 345, 17, 20)]
+        public void ExampleInputTest(int start, int stop, int divider, int count)
+        {
+            var result = solution(start, stop, divider);
+            Assert.Equal(count, result);
+        }
+    }
+}
---
+++ BaseExsercises/PassingCars.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace BaseExsercises
+{
+    public class PassingCars
+    {
+        public int solution(int[] A)
+        {
+            var left = 0;
+            foreach (var item in A)
+            {
+                if (item == 0)
+                    left++;
+            }
+
+            var pairs = 0;
+            for (var i = A.Length - 1; i >= 0; i--)
+            {
+                if (pairs > 1000000000)
+                    return -1;
+
+                var current = A[i];
+                if (current == 0)
+                {
+                    left--;
+                    continue;
+                }
+
+                pairs += left;
+            }
+
+            return pairs;
+        }
+
+        [Theory]
+        [InlineData(new[] { 0, 1, 0, 1, 1 }, 5)]
+        public void ExampleInputTest(int[] input, int count)
+        {
+            var result = solution(input);
+            Assert.Equal(count, result);
+        }
+    }
+}
