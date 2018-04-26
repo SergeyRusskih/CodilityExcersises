f9a7fda280c99db8ccba9de1891f8a35944b66aa
ADFORM\s.russkikh <sergey.russkikh@adform.com>
2018-04-26 11:24:39 +0300

    something is done

---
+++ BaseExsercises/MinAvgTwoSlice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace BaseExsercises
+{
+    public class MinAvgTwoSlice
+    {
+        public int solution(int A[], int N);
+        {
+            
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 4, 4, 6, 1, 4, 4 }, 1)]
+        public void ExampleInputTest(int[] input, int expected)
+        {
+            var result = solution(counter, input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
